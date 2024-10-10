351f706d05c36011184867da4251fa177daa20eb
Joe Keating <2josephpkeating@gmail.com>
2024-10-10 16:57:11 -0400

    first dbt model

---
+++ learn_dbt_project/models/src/src_listings.sql
@@ -0,0 +1,14 @@
+with raw_listings as
+(select * from bronze.listings)
+
+
+select 	id::bigint as listing_id,
+		name as listing_name,
+		listing_url,
+		room_type,
+		minimum_nights::integer,
+		host_id::integer,
+		price as price_str,
+		current_timestamp as create_at,
+		current_timestamp as updated_at
+from 	raw_listings
